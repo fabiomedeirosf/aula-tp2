716ceb735b0006ff77d953abb967b1dfe686b58f
Fabio Faria <fabio.faria@luizalabs.com>
2023-05-17 22:22:45 -0300

    feat: observer pattern

--- src/br/edu/fatec/padroes/observer/exvaga/Subject.java
+++ src/br/edu/fatec/padroes/observer/exvaga/Subject.java
@@ -2,7 +2,7 @@ package br.edu.fatec.padroes.observer.exvaga;
 
 public interface Subject {
 
-public void registerObserver(VagaObserver observer);
+	public void registerObserver(VagaObserver observer);
 	
 	public void removeObserver(VagaObserver observer);
 	
